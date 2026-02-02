ea1bd08296b5f155d35e473fba900d20c7cd1903
Abdellah <abdellahmoiuda@gmail.com>
2026-02-02 17:28:25 +0100

    Nav Bar Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>Hello JEDEL !!</div>;
+import Image from "next/image";
+import React, { useState } from "react";
+import jedel from "@/public/Jedel log .png";
+import Link from "next/link";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+// import Menu from "@/components/Menu";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+const HomePage = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <div>
+      <nav>
+        <Image src={jedel} alt="jedel img" className="logo"></Image>
+        <Bars3Icon
+          className="text-primary w-9 cursor-pointer sm:hidden"
+          onClick={() => setShowMenu(true)}
+        ></Bars3Icon>
+        <div className="navigation hidden sm:flex">
+          <Link className="nav-link" href={"#"}>
+            Home
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Feature
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Trusted By{" "}
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Contact us
+          </Link>
+        </div>
+
+        <menu
+          className={
+            "sm:hidden nav-menu trans " + (showMenu ? "show-menu" : "")
+          }
+        >
+          <XMarkIcon
+            onClick={() => setShowMenu(false)}
+            width={30}
+            className="absolute right-8 top-8 cursor-pointer"
+          ></XMarkIcon>
+          <Link className="nav-link" href={"#"}>
+            Home
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Feature
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Trusted By{" "}
+          </Link>
+          <Link className="nav-link" href={"#"}>
+            Contact us
+          </Link>
+        </menu>
+      </nav>
+    </div>
+  );
 };
 
-export default page;
+export default HomePage;
